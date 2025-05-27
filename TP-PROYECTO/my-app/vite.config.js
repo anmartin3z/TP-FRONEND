8f53e3d719d181837acc76d8bbdcc6baf0cfaed1
matiasjac <matiaspavon9@gmail.com>
2025-05-26 23:57:04 -0300

    Configuraciones de los proyectos, agregado de docker file y docker-compose para despliegue monolito

--- TP-PROYECTO/my-app/vite.config.js
+++ TP-PROYECTO/my-app/vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000', // Proxy para redirigir API al backend
-    },
-  },
 })
--- TP-PROYECTO/my-backend/src/index.js
+++ TP-PROYECTO/my-backend/src/index.js
@@ -5,16 +5,29 @@ import dotenv from 'dotenv';
 import routes from './routes/index.js';
 import { doFilter } from './filter.js';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(cors());
 app.use(express.json());
 app.use(doFilter)
 
 app.use('/api', routes);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
